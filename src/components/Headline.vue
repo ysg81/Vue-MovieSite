e9f86460b7bb68a7fd21bd815e28c46426f47804
ysg81 <qpple6@naver.com>
2021-05-25 23:22:35 +0900

    헤드라인 제작

---
+++ src/components/Headline.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1>
+      <span>OMDb API</span><br/>
+      The Open Movie Database<br/>
+    </h1>
+
+    <p>
+      The OMDb API is a RESTful web service to obtain movie information, all content and images on the site are contributed and maintained by our users.<br/>
+    if you find the service useful, please consider making a ont-time donation or become a patron. 
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  padding-top: 40px;
+}
+h1 {
+  line-height: 1;
+  font-family: 'Oswald', sans-serif;
+  font-size: 80px;
+}
+h1 span{
+  color: red;
+}
+p {
+  margin: 30px 0;
+  color: gray;
+}
+</style>
--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,6 +1,17 @@
 <template>
+  <Headline />
   <h1>홈페이지</h1>
   <div class="btn btn-primary">
     Home
   </div>
 </template>
+
+<script>
+import Headline from '../components/Headline'
+
+export default {
+  components: {
+    Headline
+  }
+}
+</script>
