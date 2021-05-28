7df098519b00966b8e34839b56fd3ccb670d8767
ysg81 <qpple6@naver.com>
2021-05-28 15:44:34 +0900

    api 연동

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,7 +4,9 @@
       v-model="title"
       class="form-control"
       type="text"
-      placeholder="Search for Movies, Series && more">
+      placeholder="Search for Movies, Series && more"
+      @keyup.enter="apply"  
+    >
     <div class="selects">
       <select
         v-for="filter in filters"
@@ -23,10 +25,17 @@
         </option>
       </select>
     </div>
+    <button 
+      class="btn btn-primary"
+      @click="apply"
+      >
+      Apply
+    </button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
@@ -56,6 +65,14 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    async apply() {
+      // Search movies
+      const OMDB_API_KEY = '4f15f74'
+      const res = await axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=${OMDB_API_KEY}&s=${this.title}&type=${this.type}&y=${this.year}&page=1`)
+      console.log(res)
+    }
   }
 }
 </script>
@@ -74,5 +91,12 @@ export default {
 .container .selects select{
   width: 120px;
   margin: 0 3px;
+}
+.container .btn{
+  width: 120px;
+  height: 50px;
+  font-weight: 700;
+  flex-shrink: 0;
+  margin-left: 3px;
 }
 </style>
